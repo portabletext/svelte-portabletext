1bf8408b3479dc427346f0eedd1e02255ea28925
Henrique Doro <opensource@hdoro.dev>
2022-02-17 19:47:55 -0300

    fix: pass proper `value` to ListRenderer in BlockRenderer

--- src/lib/BlockRenderer.svelte
+++ src/lib/BlockRenderer.svelte
@@ -39,7 +39,8 @@
         <ListRenderer
           portableText={{
             ...portableText,
-            list: child
+            // @ts-expect-error
+            value: child
           }}
         />
       {:else}
