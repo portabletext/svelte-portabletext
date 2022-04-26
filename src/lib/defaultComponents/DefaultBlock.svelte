64c2c6f357978323f2fcd626857a2c3446b6448b
Henrique Doro <opensource@hdoro.dev>
2022-04-26 10:54:14 -0300

    refactor: make use of svelte:element

--- src/lib/defaultComponents/DefaultBlock.svelte
+++ src/lib/defaultComponents/DefaultBlock.svelte
@@ -8,20 +8,8 @@
   $: style = value.style || 'normal'
 </script>
 
-{#if style === 'h1'}
-  <h1><slot /></h1>
-{:else if style === 'h2'}
-  <h2><slot /></h2>
-{:else if style === 'h3'}
-  <h3><slot /></h3>
-{:else if style === 'h4'}
-  <h4><slot /></h4>
-{:else if style === 'h5'}
-  <h5><slot /></h5>
-{:else if style === 'h6'}
-  <h6><slot /></h6>
-{:else if style === 'blockquote'}
-  <blockquote><slot /></blockquote>
+{#if ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote'].includes(style)}
+  <svelte:element this={style}><slot /></svelte:element>
 {:else if style === 'normal'}
   <p><slot /></p>
 {:else}
