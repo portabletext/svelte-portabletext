ae11cf88ae0589ac291f6d541007729a51e4c3e1
ampled <korsneseirik@gmail.com>
2024-11-09 13:49:48 +0100

    fix tests

--- tests/render.test.ts
+++ tests/render.test.ts
@@ -2,6 +2,7 @@
 
 import {cleanup, render} from '@testing-library/svelte/svelte5'
 import {format} from 'prettier'
+import parsers from 'prettier/parser-html'
 import {test, expect} from 'vitest'
 
 import PortableText from '../src/lib/PortableText.svelte'
@@ -10,16 +11,21 @@ import allTests from './render-tests'
 
 for (const renderTest of allTests) {
   test(renderTest.name, async () => {
+    expect(5).toBe(5)
     const renderedByPackage = render(PortableText, {
       value: renderTest.value,
       components: renderTest.components
     }).container.innerHTML
-    // cleanup()
-    // const expectedRender = render(renderTest.rendered).container.innerHTML
+    cleanup()
+    const expectedRender = render(renderTest.rendered).container.innerHTML
 
-    // expect(await format(renderedByPackage, {parser: 'html'})).toBe(
-    //   await format(expectedRender, {parser: 'html'})
-    // )
+    expect(
+      // @TODO figure out why rendered output is full of empty comments
+      await format(renderedByPackage.replaceAll('<!---->', ''), {
+        parser: 'html',
+        plugins: [parsers]
+      })
+    ).toBe(await format(expectedRender, {parser: 'html', plugins: [parsers]}))
   })
 }
 
