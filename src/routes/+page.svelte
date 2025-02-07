82026668df38e213991d2de621a36f03c21b58a1
ampled <korsneseirik@gmail.com>
2025-02-07 11:32:08 +0100

    cleanup unneeded code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import PortableText from '$lib/PortableText.svelte'
-  import type {Component, SvelteComponent} from 'svelte'
   import homepage from '../dummyData/homepage'
-
-  let comp = $state<SvelteComponent<any>>()
 </script>
 
-<PortableText bind:this={comp} value={homepage} />
+<PortableText value={homepage} />
